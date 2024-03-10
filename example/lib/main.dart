9de313d5c21b5389a10010436ef39a69e6438c3b
gnassro <gnassro@outlook.com>
2024-03-10 15:26:18 +0100

    update example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,13 +1,25 @@
+import 'dart:math' as math;
 import 'package:datetime_loop/datetime_loop.dart';
-import 'package:example/page.dart';
 import 'package:flutter/material.dart';
+import 'package:group_button/group_button.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  final GroupButtonController _controller =
+      GroupButtonController(selectedIndex: 0);
+
+  final ValueNotifier<TimeUnit> _timeUnitNotifier =
+      ValueNotifier(TimeUnit.seconds);
 
   @override
   Widget build(BuildContext context) {
@@ -15,49 +27,29 @@ class MyApp extends StatelessWidget {
       home: SafeArea(
         child: Scaffold(
           body: Center(
-            child: Builder(
-              builder: (context) {
-                return Column(
-                  children: [
-                    ElevatedButton(
-                      onPressed: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(builder: (context) => const ExamplePage(timeUnit: TimeUnit.seconds,)),
-                        );
-                      },
-                      child: const Text('Seconds'),
-                    ),
-                    ElevatedButton(
-                      onPressed: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(builder: (context) => const ExamplePage(timeUnit: TimeUnit.minutes,)),
-                        );
-                      },
-                      child: const Text('Minutes'),
-                    ),
-                    ElevatedButton(
-                      onPressed: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(builder: (context) => const ExamplePage(timeUnit: TimeUnit.hours,)),
-                        );
-                      },
-                      child: const Text('Hours'),
-                    ),
-                    ElevatedButton(
-                      onPressed: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(builder: (context) => const ExamplePage(timeUnit: TimeUnit.days,)),
-                        );
-                      },
-                      child: const Text('Days'),
-                    ),
-                  ],
-                );
-              }
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                GroupButton<TimeUnit>(
+                  controller: _controller,
+                  buttons: TimeUnit.values,
+                  onSelected: (timeUnit, index, isSelected) {
+                    _timeUnitNotifier.value = timeUnit;
+                  },
+                  buttonTextBuilder: (__, timeUnit, context) {
+                    return timeUnit.name;
+                  },
+                ),
+                const SizedBox(
+                  height: 12,
+                ),
+                ValueListenableBuilder<TimeUnit>(
+                    valueListenable: _timeUnitNotifier,
+                    builder: (context, timeUnit, __) {
+                      return ExampleComponent(timeUnit: timeUnit);
+                    })
+              ],
             ),
           ),
         ),
@@ -66,3 +58,40 @@ class MyApp extends StatelessWidget {
   }
 }
 
+class ExampleComponent extends StatelessWidget {
+  final TimeUnit timeUnit;
+
+  const ExampleComponent({super.key, required this.timeUnit});
+
+  @override
+  Widget build(BuildContext context) {
+    return DateTimeLoopBuilder(
+      timeUnit: timeUnit,
+      triggerOnStateChange: true,
+      builder: (context, dateTime, child) {
+        return Column(
+          children: [
+            Container(
+              width: 200,
+              height: 200,
+              color: Color((math.Random().nextDouble() * 0xFFFFFF).toInt())
+                  .withOpacity(1.0),
+            ),
+            Text('${dateTime.hour}:${dateTime.minute}:${dateTime.second}'),
+            child!
+          ],
+        );
+      },
+      child: Container(
+        width: 200,
+        height: 200,
+        color: Color((math.Random().nextDouble() * 0xFFFFFF).toInt())
+            .withOpacity(1.0),
+        child: const Center(
+          child: Text(
+              'Widget will not be affected by the change of the DateTimeLoopBuilder state. (Color will changed only if the state of ExampleComponent is changed)'),
+        ),
+      ),
+    );
+  }
+}
